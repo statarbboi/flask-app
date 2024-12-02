88cc834a571c6dbf645743a6e92f98eebd781954
statarbboi <stealthhft@gmail.com>
2024-12-01 21:37:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,26 +10,47 @@ CARD_TEMPLATE = """
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Get Well Soon</title>
     <style>
+        @import url('https://fonts.googleapis.com/css2?family=Comic+Neue:wght@700&display=swap');
+
         body {
-            font-family: 'Comic Sans MS', cursive, sans-serif;
-            background: linear-gradient(135deg, #ffdde1, #ee9ca7, #ff758c);
-            background-size: 400% 400%;
+            font-family: 'Comic Neue', cursive, sans-serif;
+            margin: 0;
+            overflow: hidden;
             display: flex;
             justify-content: center;
             align-items: center;
             height: 100vh;
-            margin: 0;
-            animation: gradientAnimation 6s infinite;
+            background-color: #f7f7f7;
+            position: relative;
         }
 
-        @keyframes gradientAnimation {
-            0% { background-position: 0% 50%; }
-            50% { background-position: 100% 50%; }
-            100% { background-position: 0% 50%; }
+        /* Google Dinosaur Background */
+        .background {
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: url('https://i.imgur.com/XOckS1I.png') repeat-x;
+            animation: moveBackground 5s linear infinite;
+        }
+
+        @keyframes moveBackground {
+            0% { background-position: 0 0; }
+            100% { background-position: -1200px 0; }
+        }
+
+        .dino {
+            position: absolute;
+            bottom: 10px;
+            left: 50px;
+            width: 100px;
+            height: auto;
         }
 
         .card {
-            background-color: white;
+            z-index: 10;
+            background-color: rgba(255, 255, 255, 0.9);
             border-radius: 20px;
             box-shadow: 0 10px 20px rgba(0, 0, 0, 0.2);
             padding: 20px;
@@ -61,11 +82,6 @@ CARD_TEMPLATE = """
             }
         }
 
-        p {
-            font-size: 1.2rem;
-            color: #555555;
-        }
-
         img {
             width: 200px;
             height: auto;
@@ -76,6 +92,8 @@ CARD_TEMPLATE = """
     </style>
 </head>
 <body>
+    <div class="background"></div>
+    <img class="dino" src="https://i.imgur.com/OVWHeY0.png" alt="Dino">
     <div class="card">
         <h1>Get better soon Shiho!!</h1>
         <img src="https://media.giphy.com/media/ZyoecFrXpqKwDkoCW0/giphy.gif?cid=ecf05e4794e1b65jx41ot7hzakdv3bgskior6ngth5st322x&ep=v1_gifs_search&rid=giphy.gif&ct=g" alt="Feel Better Soon">
